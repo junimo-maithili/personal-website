82e228b6270ed4990d835d9ea114ffa3943cf3bb
Maithili Rastogi <rastogim2948@gmail.com>
2025-08-24 21:04:42 -0400

    Added basic info

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ExperienceItem from './components/ExperienceItem'
+import resume from './assets/resume.pdf'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>Maithili Rastogi</h1>
-        <img>image of me here!</img>
-        <br/>
+     
+     <div>
+     <h1>Maithili Rastogi</h1>
+     <a href="https://github.com/junimo-maithili">git</a>
+     <a href="https://www.linkedin.com/in/maithili-rastogi-982938302/">linkedin</a>
+     <a href={resume} target="_blank" rel="noopener noreferrer">resume</a>
+
+        <img src="https://i.pinimg.com/736x/1c/a8/f0/1ca8f048104218a51287a1441b7eaeac.jpg"></img>
+
+
         <h2>About Me</h2>
-          <p>My name is Maithili Rastogi, and I'm a grade 12 student in Halton. I'm interesting in drawing, reading, and spending time with my friends!</p>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>My name is Maithili Rastogi, and I'm a grade 12 student in Halton. I'm interesting in drawing, reading, and spending time with my friends!</p>
+        
+        <h2>Experience</h2>
+        <ExperienceItem />
+        <ExperienceItem />
+        <ExperienceItem />
+        
+    
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   
     </>
   )
 }
---
+++ src/components/ExperienceItem.tsx
@@ -0,0 +1,20 @@
+
+const ExperienceItem = () => {
+  return (
+
+    <div className="experienceItem">
+      <h3>Co-Director of STEM from Scratch</h3>
+      <p>Nov. 2024 -- Present</p>
+      <ul>
+        <li>Executive director of STEM from Scratch, an organization dedicated to providing access to STEM opportunities to
+students, primarily in elementary school. This is achieved through bimonthly newsletters, workshops and Instagram</li>
+posts
+        <li>Designed and led a workshop to introduce students to Scratch and guided them through training a machine</li>
+learning model using Google Teachable Machine
+        <li>Led beginner and intermediate Python workshops to introduce students to the basics of Python</li>
+      </ul>
+    </div>
+  )
+}
+
+export default ExperienceItem
