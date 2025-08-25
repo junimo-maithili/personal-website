ab1986f77865a32f34d548e6f818d40920626653
Maithili Rastogi <rastogim2948@gmail.com>
2025-08-25 01:08:12 -0400

    Added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,61 @@
 import './App.css'
+import { useEffect, useRef } from "react";
 import ExperienceItem from './components/ExperienceItem'
 import resume from './assets/resume.pdf'
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Typed from "typed.js";
+import myImg from "./assets/myImg.JPG"
 
 function App() {
 
-  const myImg = document.querySelector('.myImg');
+  /* Typing animation for name */
+  const el = useRef<HTMLSpanElement>(null);
+    useEffect(() => {
+      const typed = new Typed(el.current!, {
+        strings: ["Maithili Rastogi"],
+        typeSpeed: 80,
+        backSpeed: 0,
+      });
+    
+      return () => {
+        typed?.destroy();
+      };
+    }, []);
 
-  myImg?.addEventListener('mouseenter', () => {
-    myImg.classList.add('rotated');
-});
+  
 
-setTimeout(() => {
-  myImg?.classList.remove('rotated');
-}, 4000);
 
 AOS.init({
-  duration: 800, // animation speed
-  once: true // only animate once
+  duration: 800,
+  once: true
 });
-
-
-
 
   return (
     <>
      
      <div>
-     <h1 className="name"><span id="typed">Maithili Rastogi</span></h1>
+      
+     <h1><span ref={el}></span></h1>;
      <div className="contactLinks">
-      <a href="https://github.com/junimo-maithili">git</a>
+      <a href="https://github.com/junimo-maithili">github</a>
       <a href="https://www.linkedin.com/in/maithili-rastogi-982938302/">linkedin</a>
-      <a href={resume} target="_blank" rel="noopener noreferrer">resume</a>
+      <a href={resume} target="_blank" rel="noopener noreferrer">resumé</a>
      </div>
      <br/>
 
     <div className="myImgDiv">
       <img className="smallImg" src="https://i.pinimg.com/736x/1c/a8/f0/1ca8f048104218a51287a1441b7eaeac.jpg"></img>
-      <img className="myImg" src="https://i.pinimg.com/736x/1c/a8/f0/1ca8f048104218a51287a1441b7eaeac.jpg"></img>
+      <img className="myImg" src={myImg}></img>
       <img className="smallImg" src="https://i.pinimg.com/736x/1c/a8/f0/1ca8f048104218a51287a1441b7eaeac.jpg"></img>
     </div>
 
 
-        <div className="aboutMe">
+        <div className="aboutMe" data-aos="flip-right">
           <h2>About Me</h2>
-          <p>My name is Maithili Rastogi, and I'm a grade 12 student in Halton. I'm interested in drawing, reading, and spending time with my friends!</p>
+          <p>I'm Maithili, and I'm a grade 12 student in Halton. I'm interested in drawing, gaming, and spending time with my friends!</p>
         </div>
-        <div className="experience" data-aos="fade-up">
+        <div className="experience" data-aos="flip-left">
           <h2>Experience</h2>
           
           <ExperienceItem />
@@ -55,7 +63,7 @@ AOS.init({
           <ExperienceItem />
         </div>
 
-        <div className="projectDiv">
+        <div className="projectDiv" data-aos="flip-right">
           <h2>Projects</h2>
  
           <div className="projects">
